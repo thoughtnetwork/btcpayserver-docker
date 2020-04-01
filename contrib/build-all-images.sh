5a6da50fc9bca9a783dacec51c9e88e7677feeb0
Andrew J Hacker <andrew@thought.live>
2020-04-01 10:10:00 -0400

    dl fix

--- contrib/build-all-images.sh
+++ contrib/build-all-images.sh
@@ -785,10 +785,10 @@ DOCKERFILE="Thought/0.18.0/linuxamd64.Dockerfile"
 [[ "$(uname -m)" == "armv7l" ]] && DOCKERFILE="Thought/0.18.0/linuxarm32v7.Dockerfile"
 # https://raw.githubusercontent.com/btcpayserver/dockerfile-deps/Thought/0.18.0/Thought/0.18.0/linuxarm64v8.Dockerfile
 [[ "$(uname -m)" == "aarch64" ]] && DOCKERFILE="Thought/0.18.0/linuxarm64v8.Dockerfile"
-echo "Building btcpayserver/thought:0.18.0"
+echo "Building btcpayserver/Thought:0.18.0"
 git clone https://github.com/thoughtnetwork/dockerfile-deps thought
 cd thought
 git checkout Thought/0.18.0
 cd "$(dirname $DOCKERFILE)"
-docker build -f "$DOCKERFILE" -t "btcpayserver/thought:0.18.0" .
+docker build -f "$DOCKERFILE" -t "btcpayserver/Thought:0.18.0" .
 cd - && cd ..
